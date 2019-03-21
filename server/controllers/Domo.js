c542abc76896d92c32ce88db304c593217b6c4e8
JohnWSMartel <jwm3464@g.rit.edu>
2019-03-21 16:18:15 -0400

    Minor fixes

--- server/controllers/Domo.js
+++ server/controllers/Domo.js
@@ -1,16 +1,16 @@
-const makerPage = (req, res) => {
-  Domo.DomoModel.findByOwner(req.session.account._id, (err, docs) => {
-		if(err){
-			console.log(err);
-			return res.status(400).json({ error: 'An error occured'});
-		}
-		return res.render('app', {domos: docs});
-	});
-};
-
 const models = require('../models');
 
 const Domo = models.Domo;
+
+const makerPage = (req, res) => {
+  Domo.DomoModel.findByOwner(req.session.account._id, (err, docs) => {
+    if (err) {
+      console.log(err);
+      return res.status(400).json({ error: 'An error occured' });
+    }
+    return res.render('app', { domos: docs });
+  });
+};
 
 const makeDomo = (req, res) => {
   if (!req.body.name || !req.body.age) {
